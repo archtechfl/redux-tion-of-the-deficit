f29fb230640294af08b4668480ba237c8d22bf79
Jeremy Moore <archtechfl@gmail.com>
2017-11-28 12:23:55 -0500

    preparing color mixer

--- app/colorMixer.jsx
+++ app/colorMixer.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 
 export default class ColorMixer extends React.Component {
 
+    constructor(props){
+        super(props);
+    }
+
     componentDidMount(){
         console.log("Color mixer mounted");
     }
@@ -10,6 +14,31 @@ export default class ColorMixer extends React.Component {
         return (
             <div className="color-mixer-wrapper">
                 <h3>Color Controls</h3>
+                <div className="rgb-inputs">
+                    <div className="red-input">
+                        <p>Red</p>
+                        <input type="number" name="red" min="0" max="255" />
+                    </div>
+                    <div className="green-input">
+                        <p>Green</p>
+                        <input type="number" name="green" min="0" max="255" />
+                    </div>
+                    <div className="blue-input">
+                        <p>Blue</p>
+                        <input type="number" name="blue" min="0" max="255" />
+                    </div>
+                    <div className="hex-code">
+                        <p>Hex code</p>
+                        <input type="text" name="hex" placeholder="FFFFFF" />
+                    </div>
+                </div>
+                <div className="color-name">
+                    <p>Color name: <span className="color-name-value"></span></p>
+                </div>
+                <div className="color-square">
+                    <p>Color square</p>
+                    <div className="color-swatch"></div>
+                </div>
             </div>
         );
     }
